f224155ac4dfd18a7db65e7bce3e1ebe62eaf63c
Wongani <wonganichulu@gmail.com>
2024-08-04 09:03:41 +0200

    Made all .py file in api executable

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,12 +3,30 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Returns API status code"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Retrieves the number of each objects by type"""
+    objs = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    statistics = {}
+    for key, value in objs.items():
+        statistics[key] = storage.count(value)
+    return jsonify(statistics)
 
 
 if __name__ == "__main__":
